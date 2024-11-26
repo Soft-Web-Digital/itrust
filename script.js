7c45a4de5cce55c4f1a423b2cfd6a8fc2c8b8138
theactualdev <olayinkacodes@gmail.com>
2024-11-26 12:39:16 +0100

    fixes mobile menu

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const faq = document.querySelectorAll('.q .visible img');
+const drops = document.querySelectorAll('.drop');
+const ham = document.querySelector('.ham');
+const dropMenu = document.querySelector('.drop-menu')
 
 faq.forEach((question) => {
     question.addEventListener('click', () => {
@@ -13,3 +16,26 @@ faq.forEach((question) => {
         }
     });
 });
+
+drops.forEach((drop) => {
+    drop.addEventListener(('click'), () => {
+        const dropdown = drop.closest('.drop').querySelector('.dropdown');
+        if(dropdown.classList.contains('hidden')){
+            dropdown.classList.remove('hidden');
+            dropdown.classList.add('flex');
+        }else{
+            dropdown.classList.add('hidden');
+            dropdown.classList.remove('flex');
+        }
+    })
+})
+
+ham.addEventListener(('click'), () => {
+    if(dropMenu.classList.contains('hidden')){
+        dropMenu.classList.remove('hidden');
+        dropMenu.classList.add('flex');
+    }else{
+        dropMenu.classList.add('hidden');
+        dropMenu.classList.remove('flex');
+    }
+})
