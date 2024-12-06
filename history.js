5712e6e99abb9af24f555c220be57e0b60b7469e
theactualdev <olayinkacodes@gmail.com>
2024-12-06 16:25:45 +0100

    finishing touches

---
+++ history.js
@@ -0,0 +1,35 @@
+const switchTabs = document.querySelector('.switch')
+const tabss = switchTabs.querySelectorAll('p')
+
+tabss.forEach((tab) => {
+    tab.addEventListener('click', () => {
+        tabss.forEach((t) => {
+            t.classList.remove('active-history')
+        })
+        tab.classList.add('active-history')
+    })
+})
+
+const trade = document.querySelector('.trades-history');
+const transaction = document.querySelector('.transaction-history');
+const savings = document.querySelector('.savings-history');
+
+tabss.forEach((tab) => {
+    tab.addEventListener('click', () => {
+        const sections = [trade, transaction, savings];
+        sections.forEach((section) => {
+            section.classList.remove('flex');
+            section.classList.add('hidden');
+        })
+        if(tab.textContent === "Trade History"){
+            trade.classList.add('flex');
+            trade.classList.remove('hidden');
+        }else if(tab.textContent === "Transaction History"){
+            transaction.classList.add('flex');
+            transaction.classList.remove('hidden');
+        }else if(tab.textContent === "Savings History"){
+            savings.classList.add('flex');
+            savings.classList.remove('hidden')
+        }
+    })
+})
